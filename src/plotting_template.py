d0ebb2b70dbd2ae96bf47ad573b8e17a7622310a
stefankenzan <92782220+stefankenzan@users.noreply.github.com>
2025-10-17 17:24:56 +0100

    Formatting kinda sorted, will see how it goes moving forward. Started on intro, not finished yet. Benching my work on chapter 5 as a .jank file so it doesn't get incorportated into the build calls. Currently "jupyter-book build ." only will build .ipynb files so that it doesn't error!

---
+++ src/plotting_template.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+lw = 1.5
+plt.rcParams['lines.linewidth'] = lw
+plt.rcParams['patch.linewidth'] = lw
+plt.rcParams['font.size'] = 14
+plt.rcParams['figure.figsize'] = (5, 4)
